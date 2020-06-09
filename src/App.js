aa30aa38781cd1240145aae773b8d850edb1ff7a
Jeffrey Meus <jeffrey.meus@live.be>
2020-06-09 11:27:57 +0200

    Default app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-green-500 text-gray-100 p-4 text-center font-semibold tracking-widest">
+      Reactjs with Tailwind
+    </div>
+  );
 }
 
 export default App;
